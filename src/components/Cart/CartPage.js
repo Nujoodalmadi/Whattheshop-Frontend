83ffbf470bb654aa019f8bce5c5b270ef912bb10
Nujood AlMadhi <nujoodalmadi@gmail.com>
2019-04-03 12:04:47 +0300

    modified cart page

--- src/components/Cart/CartPage.js
+++ src/components/Cart/CartPage.js
@@ -1,38 +1,43 @@
 import React, { Component } from "react";
-import { Card, ListGroup } from "react-bootstrap";
+import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
 import CartRow from "./CartRow";
 
 class CartPage extends Component {
   render() {
     return (
-      <div>
-        <Card className=" d-flex flex-column text-center  w-75 d-flex justify-content-center mt-5 ml-5">
-          <Card.Body>
-            <Card.Title className="text-muted">Your Cart</Card.Title>
-            <ListGroup className="text-left" variant="flush">
-              <ListGroup.Item
-                style={{ fontSize: "12px", fontWeight: "bold" }}
-                className="d-flex flex-row"
-              >
-                <div className="p-2 col-5 ml-3">Item</div>
-                <div className="p-2 col-3 mr-2"> Price (each)</div>
-                <div className="p-2 col-1">Qty </div>
-                <div className="p-2">Subtotal</div>
-              </ListGroup.Item>
+      <div
+        className=" justify-content-center mt-5"
+        align="center"
+        style={{ minWidth: "960px" }}
+      >
+        <Card className="w-75">
+          <Card.Body style={{ textAlign: "center", height: "75px" }}>
+            <Card.Title className="text-muted" style={{ fontSize: "25px" }}>
+              Your Cart
+            </Card.Title>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroup.Item
+              style={{ fontSize: "12px", fontWeight: "bold" }}
+              className="d-flex flex-row"
+            >
+              <div className="p-2 col-5 ml-2">Item</div>
+              <div className="p-2 col-3 mr-2"> Price (each)</div>
+              <div className="p-2 col-1">Qty </div>
+              <div className="p-2">Subtotal</div>
+            </ListGroup.Item>
 
-              {/* Row starts here */}
-              <CartRow />
-              <CartRow />
-              <CartRow />
-            </ListGroup>
+            <CartRow />
+          </ListGroup>
+          <div className="d-flex justify-content-center">
             <button
               type="submit"
               className="btn btn-light "
-              style={{ color: "grey", marginTop: "10px" }}
+              style={{ color: "grey", margin: "10px" }}
             >
               Checkout
             </button>
-          </Card.Body>
+          </div>
         </Card>
       </div>
     );
--- src/components/Cart/CartRow.js
+++ src/components/Cart/CartRow.js
@@ -1,38 +1,40 @@
 import React, { Component } from "react";
-import { ListGroup } from "react-bootstrap";
+import { ListGroup, ListGroupItem } from "react-bootstrap";
 
 class CartRow extends Component {
   render() {
     return (
-      <ListGroup.Item className="d-flex flex-row align-items-center">
-        <img
-          src="https://pbs.twimg.com/profile_images/555038545446846464/ckLeRnHU.jpeg"
-          height="100"
-          width="100"
-          alt="Avatar"
-        />
-        <div
-          className="p-2 col-4"
-          style={{ fontSize: "15px", padding: "20px" }}
-        >
-          Cras justo odiof rfvmekr kmef
-        </div>
-        <div className="p-2 col-3" style={{ fontSize: "13px" }}>
-          15SAR
-        </div>
-        <div className="p-2 col-1" style={{ fontSize: "13px" }}>
-          5
-        </div>
-        <div className="p-2" style={{ fontSize: "13px" }}>
-          75SAR
-        </div>
+      <ListGroupItem>
+        <ListGroup.Item className="d-flex flex-row align-items-center">
+          <img
+            src="https://pbs.twimg.com/profile_images/555038545446846464/ckLeRnHU.jpeg"
+            height="100"
+            width="100"
+            alt="Avatar"
+          />
+          <div
+            className="p-2 col-4"
+            style={{ fontSize: "15px", padding: "20px" }}
+          >
+            Cras justo odiof rfvmekr kmef
+          </div>
+          <div className="p-2 col-3" style={{ fontSize: "13px" }}>
+            15SAR
+          </div>
+          <div className="p-2 col-1" style={{ fontSize: "13px" }}>
+            5
+          </div>
+          <div className="p-2" style={{ fontSize: "13px" }}>
+            75SAR
+          </div>
 
-        <i
-          onClick={() => alert("hi")}
-          className="ml-auto p-2 far fa-trash-alt"
-          style={{ color: "rgb(155, 166, 87)", position: "relative" }}
-        />
-      </ListGroup.Item>
+          <i
+            onClick={() => alert("hi")}
+            className="ml-auto p-2 far fa-trash-alt"
+            style={{ color: "rgb(155, 166, 87)", position: "relative" }}
+          />
+        </ListGroup.Item>
+      </ListGroupItem>
     );
   }
 }
