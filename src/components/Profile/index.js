dbc62944d3113d68742f319dd8bf17b556a4863e
Modi91 <alotaibimood@gmail.com>
2019-04-07 11:05:35 +0300

    fetch orders

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,9 +1,27 @@
 import React, { Component } from "react";
+import * as actionCreators from "../../store/actions";
+import { connect } from "react-redux";
 
 class index extends Component {
+  componentDidMount() {
+    this.props.fetchOrders();
+  }
+
   render() {
+    const orders = this.props.orders;
+    console.log("TCL: index -> render -> orders", orders);
     return <div />;
   }
 }
 
-export default index;
+const mapStateToProps = state => ({
+  orders: state.products.orders
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchOrders: () => dispatch(actionCreators.fetchOrders())
+});
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(index);
